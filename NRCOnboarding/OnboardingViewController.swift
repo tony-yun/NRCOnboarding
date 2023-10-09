c3da94feb8b110a82cd3aaf1937450b2465e0847
tony-yun <lifeyun24@naver.com>
2023-10-09 23:14:00 +0900

    setting basic view controller

---
+++ NRCOnboarding/OnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboarding
+//
+//  Created by 윤태웅 on 10/9/23.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
